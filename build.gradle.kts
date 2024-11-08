2343d6c27c931473d0fa18c1ffab5ec79b3dd377
tomondre <tomas.ondrejka2000@gmail.com>
2024-11-08 16:25:09 +0100

    * add jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.auth0:java-jwt:4.4.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
