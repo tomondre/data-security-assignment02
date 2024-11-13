6958fa508bec409f7cafc48f2df4432f3df754ac
tomondre <tomas.ondrejka2000@gmail.com>
2024-11-13 15:52:15 +0100

    Add RBAC strategy

---
+++ src/main/java/org/example/server/model/AclRole.java
@@ -0,0 +1,8 @@
+package org.example.server.model;
+
+public final class AclRole {
+    public static final String MANAGER = "manager";
+    public static final String TECHNICIAN = "technician";
+    public static final String POWER_USER = "powerUser";
+    public static final String ORDINARY_USER = "ordinaryUser";
+}
