55dad5a06be7cb4c5179327296c3aee78ad225e1
tomondre <tomas.ondrejka2000@gmail.com>
2024-11-20 21:31:54 +0100

    add users

--- src/main/java/org/example/Bootstrap.java
+++ src/main/java/org/example/Bootstrap.java
@@ -49,6 +49,21 @@ public class Bootstrap {
             System.out.println("Failed to create user table." + e.getMessage());
             throw new RemoteException("Failed to create user table", e);
         }
+        try (PreparedStatement stmt = connection.prepareStatement(
+                     "INSERT INTO users (username, hashed_password, salt) VALUES " +
+                             "('Cecilia', '61KxspHv9XfcO6TO159Cb6VGvDIy5sI19DR6k6+i4lM=', '3uPcEtO7Ujw/OGoIPdiOBA==')," +
+                             "('Bob', 'agdom3Kk60VO/xi3SHGrQ4xox0S30G8HQ5cf3+BHOjQ=', '3FtavKrg0Qy5wpwJcSlCFQ==')," +
+                             "('David', 'KU8jk+jiCAFvSl0uyKxx+nzIBw4L4FpnXW4eU9lklrU=', '5rANVmSwYmaVzWzSKGS22g==')," +
+                             "('Alice', 'BRABucAUJ2BHVy+V1g5r9x/xENj2enpr4f0RaMqUyIc=', 'Qa9C1jD60oRd3s8fN9hINw==')"
+             )) {
+            stmt.executeUpdate();
+            System.out.println("Users created successfully");
+        } catch (SQLException e) {
+            System.out.println("Failed to create users." + e.getMessage());
+            throw new RemoteException("Failed to create users", e);
+        }
+
+
         try {
             this.authorizationStrategy.bootstrapDb(connection);
             System.out.println("ACL table created successfully");
